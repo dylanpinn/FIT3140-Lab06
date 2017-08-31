c82fdb359bb93e721f1673a4a53fdc5c396e98ba
Dylan Pinn <dylan@dylanpinn.com>
2017-09-01 08:28:57 +1000

    Add updateData function

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,12 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.updateData = functions.database.ref(`channel/{id}`).onWrite(event => {
+    const msgKey = event.data.key;
+    const msgVal = event.data.val();
+    console.log(`Key: ${event.data.key}`);
+    console.log(`Data: ${JSON.stringify(msgVal)}`);
+    const myNewValue = `${msgVal.data} new Update`;
+    return event.data.ref.child('newAtt').set(myNewValue);
+})
