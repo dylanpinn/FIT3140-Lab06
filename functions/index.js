cbb7865bb31a1deb997cba920f4840cdfceadc08
Dylan Pinn <dylan@dylanpinn.com>
2017-09-01 08:30:09 +1000

    Add helloWorld function

--- functions/index.js
+++ functions/index.js
@@ -2,10 +2,6 @@ const functions = require('firebase-functions');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
 
 exports.updateData = functions.database.ref(`channel/{id}`).onWrite(event => {
     const msgKey = event.data.key;
@@ -15,3 +11,7 @@ exports.updateData = functions.database.ref(`channel/{id}`).onWrite(event => {
     const myNewValue = `${msgVal.data} new Update`;
     return event.data.ref.child('newAtt').set(myNewValue);
 })
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
